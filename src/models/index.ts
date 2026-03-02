ed144c85a4c88b20c79c5e799a0a4904394e5dc2
Diego Reyes <diegorele13@gmail.com>
2026-03-02 09:00:27 -0500

    add: models

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import { UserModel } from "./user.model";
+import { WorkspaceModel } from "./workspace.model";
+
+const models = {
+  users: UserModel,
+  workspaces: WorkspaceModel,
+};
+
+export default models;
