0f2eed9e19badd97718b2e0b32e51c580c314144
Diego Reyes <diegorele13@gmail.com>
2026-03-02 14:34:14 -0500

    add: route workspace

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express, { Application } from "express";
 import authRouter from "./auth.router";
+import workspaceRouter from "./workspace.router";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api", router);
 
   router.use("/auth", authRouter);
+  router.use("/workspaces", workspaceRouter);
 }
 
 export default routerApi;
